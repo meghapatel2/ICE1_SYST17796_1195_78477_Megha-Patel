7b6a2b4163e047b57a80aeb08fb5809829c56ae7
Angela Villadiego <angie.vdiego@gmail.com>
2019-05-21 15:58:33 -0400

    Added name and student number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Student Number: 991556560
+ * Name: Angela Villadiego
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
