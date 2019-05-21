80ed2d19e278bba554e39d7f3254b93e90ef6755
zNostalgia <amanpreet.singh.0719@gmail.com>
2019-05-21 15:56:47 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -35,6 +35,9 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             //Then report the result here
+            
+            c.setValue(2);
+            c.setSuit(Card.SUITS[1]);
         }
 
     }
