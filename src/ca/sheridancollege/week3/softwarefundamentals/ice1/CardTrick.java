e7bccd442f37237dd247f9532343ee1b2563182a
deepk <deepk@DESKTOP-DN8RF62>
2019-05-21 15:24:59 -0400

    name change in comment.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author Dipesh kabariya 
+ * @author Dipesh kabariya 991550213
  */
 public class CardTrick {
     
