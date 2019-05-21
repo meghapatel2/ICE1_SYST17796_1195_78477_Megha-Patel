0401a6844dbad39b16538b6ecf20044185bad2f8
WilsonQinWu <wuqin_04@hotmail.com>
2019-05-21 16:28:24 -0400

    increase student id

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,7 @@ import java.util.Random;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author Qin Wu
+ * @author Qin Wu studendt id 991520749
  */
 public class CardTrick
 {
