df5e3b0ecaee960de239972633407f9e262df9a5
KongJai <jailong1999@gmail.com>
2019-05-21 19:58:03 -0400

    I dont know what im doing

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author Megha
+ * @author Jailong Kong 991539148
  */
 public class CardTrick {
     
@@ -22,6 +22,11 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            int x = (int)((Math.random()*13)+1);
+            c.setValue(x);
+            int y = (int)((Math.random()*4)+1);
+            c.setSuit(SUITS[y]);
+            
             //c.setValue(insert call to random number generator here)
            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
            //Then report the result here
