d7f8359ec96bb4b61d30acc81d5b2653e141f4de
Jamal <karshe@sheridancollege.ca>
2019-05-21 15:58:26 -0400

    Edited Code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,27 +1,34 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 import java.util.Random;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
  * @author Megha
+ * @author Jamal Karshe
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-           //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-           //Then report the result here
+            Random r = new Random();
+
+            int randomNum = r.nextInt((13 - 1) + 1) + 1;
+            c.setValue(randomNum);
+
+            String rString = Card.SUITS[r.nextInt(Card.SUITS.length)];
+            c.setSuit(rString);
+            System.out.println(c.getValue() + " " + c.getSuit());
+        }
+            Card userCard=new Card();
+            
+            userCard.setSuit("Hearts");
+            userCard.setValue(2);
     }
-    
-}
 }
