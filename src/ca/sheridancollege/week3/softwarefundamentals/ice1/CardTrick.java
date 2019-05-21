f20c04faf4c18c344751adace712380ce3f8cba9
WilsonQinWu <wuqin_04@hotmail.com>
2019-05-21 16:19:39 -0400

    design code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Random;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -22,7 +22,16 @@ public class CardTrick
       for (int i = 0; i < magicHand.length; i++) {
          Card c = new Card();
          //c.setValue(insert call to random number generator here)
+         Random rnd = new Random();
+         int num = rnd.nextInt(13) + 1;
+         c.setValue(num);
          //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         String str = Card.SUITS[rnd.nextInt(Card.SUITS.length)];
+         c.setSuit(str);
+         System.out.println(c.getValue() + "" + c.getSuit());
+         Card user = new Card();
+         user.setSuit("Hearts");
+         user.setValue(2);
          //Then report the result here
       }
 
