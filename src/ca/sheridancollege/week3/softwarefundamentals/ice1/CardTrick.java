eefba9dfc1acc00829272cc123e09fa76f3cd656
Angela Villadiego <angie.vdiego@gmail.com>
2019-05-21 15:26:26 -0400

    First Commit; changed modifier name

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,6 +12,7 @@ import java.util.Random;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
+ * @modifier Angela Villadiego
  */
 public class CardTrick {
     
